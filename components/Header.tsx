897c01dc36e0e3c6075cf74ab27ba07629ff4a87
nearworld <artemismars2@outlook.com>
2024-09-25 18:32:17 +0900

    fix: Header 디자인 수정

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 export default function Header() {
   return (
     <header className='bg-white'>
-      <div className='p-2 border-b-[1px] flex items-center'>
-        <Image src='/images/logo.png' alt='logo' width={100} height={100} />
+      <div className='p-2 border-b-[1px] flex items-center gap-3'>
+        <Image src='/images/logo.png' alt='logo' width={60} height={60} />
         <h1 className='font-bold whitespace-nowrap'>
           {process.env.NEXT_PUBLIC_BUSINESS_NAME}
         </h1>
