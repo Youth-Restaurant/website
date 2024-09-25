408a34755d4f4ec1f6059d3264ba93b52517e32c
nearworld <artemismars2@outlook.com>
2024-09-25 18:28:56 +0900

    feat: 메인 페이지에 음식 이미지 슬라이드 추가

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import MainSlide from '@/components/MainSlide';
 import Header from '@/components/Header';
+import MainFoodSlide from '@/components/MainFoodSlide';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -14,6 +16,14 @@ export default function Home() {
       <Header />
       <main className='min-h-screen'>
         <MainSlide />
+        <div className='p-5 pb-0'>
+          <Link href='/food'>
+            <span className='font-semibold text-lg'>
+              음식 메뉴 확인하러 가기 {'>'}
+            </span>
+          </Link>
+        </div>
+        <MainFoodSlide />
       </main>
       <Navbar />
       <Footer />
---
+++ components/MainFoodSlide.tsx
@@ -0,0 +1,66 @@
+import Image from 'next/image';
+import { Autoplay, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+export default function MainFoodSlide() {
+  const foodImages = [
+    '기본 도시락',
+    '프리미엄 도시락',
+    '무왕 밥상',
+    '사골우거지탕',
+    '서동밥상',
+    '수육정식',
+    '왕의비빔밥',
+    '집밥',
+    '출장 뷔페',
+  ];
+  const prices = [
+    '10,000',
+    '15,000',
+    '20,000',
+    '25,000',
+    '30,000',
+    '35,000',
+    '40,000',
+    '45,000',
+    '50,000',
+  ];
+
+  return (
+    <Swiper
+      slidesPerView={'auto'}
+      spaceBetween={30}
+      pagination={{
+        clickable: true,
+      }}
+      className='mySwiper'
+      style={{
+        paddingLeft: 50,
+        paddingBottom: 50,
+      }}
+    >
+      {foodImages.map((item, index) => (
+        <SwiperSlide key={item + index} style={{ width: 300, height: 400 }}>
+          <div className='rounded-lg relative p-3 h-80 shadow-2xl overflow-hidden'>
+            <div className='relative h-64 overflow-hidden'>
+              <Image
+                src={`/images/food/${index}.png`}
+                alt={`image-${item}`}
+                width={600}
+                height={100}
+                priority
+              />
+            </div>
+            <div className='flex gap-3'>
+              <span className='font-semibold'>{item}</span>
+              <div>
+                <span className='text-red-500'>{prices[index]}</span>
+                <span>원</span>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+}
