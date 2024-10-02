02a77f766a97b9a3b60de8f0bb3b3ae8e17ef6e2
nearworld <artemismars2@outlook.com>
2024-10-02 11:28:02 +0900

    feat: tailwindcss 컬러를 타입으로 지정

---
+++ types/color.ts
@@ -0,0 +1,42 @@
+type TailwindColor =
+  | "slate"
+  | "gray"
+  | "zinc"
+  | "neutral"
+  | "stone"
+  | "red"
+  | "orange"
+  | "amber"
+  | "yellow"
+  | "lime"
+  | "green"
+  | "emerald"
+  | "teal"
+  | "cyan"
+  | "sky"
+  | "blue"
+  | "indigo"
+  | "violet"
+  | "purple"
+  | "fuchsia"
+  | "pink"
+  | "rose";
+
+type TailwindShade =
+  | "50"
+  | "100"
+  | "200"
+  | "300"
+  | "400"
+  | "500"
+  | "600"
+  | "700"
+  | "800"
+  | "900"
+  | "950";
+
+export type TailwindBgColor =
+  | `bg-${TailwindColor}-${TailwindShade}`
+  | "bg-transparent"
+  | "bg-black"
+  | "bg-white";
