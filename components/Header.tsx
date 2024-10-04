f569f98b5a89e4f7829ab35033a49b0888a1bc73
nearworld <artemismars2@outlook.com>
2024-10-04 11:19:28 +0900

    feat: 헤더 클릭시 메인페이지 이동

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,15 @@
-import Image from 'next/image';
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className='bg-white'>
-      <div className='p-2 border-b-[1px] flex items-center gap-3'>
-        <Image src='/images/logo.png' alt='logo' width={60} height={60} />
-        <h1 className='font-bold whitespace-nowrap'>
+    <header className="bg-white">
+      <Link href="/" className="p-2 border-b-[1px] flex items-center gap-3">
+        <Image src="/images/logo.png" alt="logo" width={60} height={60} />
+        <h1 className="font-bold whitespace-nowrap">
           {process.env.NEXT_PUBLIC_BUSINESS_NAME}
         </h1>
-      </div>
+      </Link>
     </header>
   );
 }
