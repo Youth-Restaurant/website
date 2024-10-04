64dd3c6e5207a3161a0c24925db4d6ebef3ed719
nearworld <artemismars2@outlook.com>
2024-10-04 11:15:11 +0900

    fix: header, footer, navbar 모두 layout 으로 이동

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 
 const pretendard = localFont({
   src: "./fonts/PretendardVariable.woff2",
@@ -20,7 +23,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${pretendard.className} antialiased`}>{children}</body>
+      <body className={`${pretendard.className} antialiased`}>
+        <div
+          id="commonContainer"
+          className="max-w-[600px] my-0 mx-auto border-x-[1px] min-w-[320px] box-content"
+        >
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          <Navbar />
+          <Footer />
+        </div>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,38 @@
 "use client";
 import "swiper/css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
 import MainSlide from "@/components/MainSlide";
-import Header from "@/components/Header";
 import MainFoodSlide from "@/components/MainFoodSlide";
 import Link from "next/link";
 import TimeCard from "@/components/TimeCard";
 
 export default function Home() {
   return (
-    <div
-      id="commonContainer"
-      className="max-w-[600px] my-0 mx-auto border-x-[1px] min-w-[320px] box-content"
-    >
-      <Header />
-      <main className="min-h-screen">
-        <MainSlide />
-        <div className="p-3">
-          <h1 className="text-lg font-semibold">운영시간</h1>
-          <div className="flex gap-3 mt-3">
-            <TimeCard
-              title="점심"
-              time="11:30 - 14:00"
-              bgColor="bg-emerald-500"
-            />
-            <div className="relative flex-1">
-              <TimeCard
-                title="예약"
-                time="11:30 - 20:00"
-                bgColor="bg-blue-400"
-              />
-              <span className="text-red-300 text-sm absolute">
-                *당일 예약 불가
-              </span>
-            </div>
+    <main className="min-h-screen">
+      <MainSlide />
+      <div className="p-3">
+        <h1 className="text-lg font-semibold">운영시간</h1>
+        <div className="flex gap-3 mt-3">
+          <TimeCard
+            title="점심"
+            time="11:30 - 14:00"
+            bgColor="bg-emerald-500"
+          />
+          <div className="relative flex-1">
+            <TimeCard title="예약" time="11:30 - 20:00" bgColor="bg-blue-400" />
+            <span className="text-red-300 text-sm absolute">
+              *당일 예약 불가
+            </span>
           </div>
         </div>
-        <div className="p-5 pb-0">
-          <Link href="/food">
-            <span className="font-semibold text-lg">
-              음식 메뉴 확인하러 가기 {">"}
-            </span>
-          </Link>
-        </div>
-        <MainFoodSlide />
-      </main>
-      <Navbar />
-      <Footer />
-    </div>
+      </div>
+      <div className="p-5 pb-0">
+        <Link href="/food">
+          <span className="font-semibold text-lg">
+            음식 메뉴 확인하러 가기 {">"}
+          </span>
+        </Link>
+      </div>
+      <MainFoodSlide />
+    </main>
   );
 }
