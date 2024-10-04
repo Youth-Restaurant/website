84c5b32821ef8722813d53d9b5504da501801d60
nearworld <artemismars2@outlook.com>
2024-10-04 15:20:40 +0900

    fix: Header에 sticky 적용하여 상단 고정

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="bg-white">
+    <header className="bg-white sticky top-0 z-50">
       <Link href="/" className="p-2 border-b-[1px] flex items-center gap-3">
         <Image src="/images/logo.png" alt="logo" width={60} height={60} />
         <h1 className="font-bold whitespace-nowrap">
